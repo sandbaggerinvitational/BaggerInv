71d241d97ca4217366b95eead0923d8b553bd694
sandbaggerinvitational <sandbaggerinvitational@gmail.com>
2026-07-21 10:02:58 -0500

    Add files via upload

--- app/history/[year]/round/[round]/page.js
+++ app/history/[year]/round/[round]/page.js
@@ -1,4 +1,5 @@
 export const dynamic = "force-dynamic";
+import Link from "next/link";
 import { refreshHistoricalData } from "../../../../../lib/stats";
 import { notFound } from "next/navigation";
 import { Header, Footer } from "../../../../components";
